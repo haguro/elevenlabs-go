4e1729ae8a9ef3468b39a2b39e9b8a09df4aabea
haguro <1076+haguro@users.noreply.github.com>
2023-06-14 20:24:30 +1000

    Move test response bodies to separate file
    ...and various of other "tweaks".

--- elevenlabs/client.go
+++ elevenlabs/client.go
@@ -47,54 +47,6 @@ func SetTimeout(timeout time.Duration) {
 	getDefaultClient().timeout = timeout
 }
 
-func TextToSpeech(voiceID string, ttsReq TextToSpeechRequest, queries ...QueryFunc) ([]byte, error) {
-	return getDefaultClient().TextToSpeech(voiceID, ttsReq, queries...)
-}
-
-func GetModels() ([]Model, error) {
-	return getDefaultClient().GetModels()
-}
-
-func GetVoices() ([]Voice, error) {
-	return getDefaultClient().GetVoices()
-}
-
-func GetDefaultVoiceSettings() (VoiceSettings, error) {
-	return getDefaultClient().GetDefaultVoiceSettings()
-}
-
-func GetVoiceSettings(voiceId string) (VoiceSettings, error) {
-	return getDefaultClient().GetVoiceSettings(voiceId)
-}
-
-func GetVoice(voiceId string, queries ...QueryFunc) (Voice, error) {
-	return getDefaultClient().GetVoice(voiceId, queries...)
-}
-
-func DeleteVoice(voiceId string) error {
-	return getDefaultClient().DeleteVoice(voiceId)
-}
-
-func EditVoiceSettings(voiceId string, settings VoiceSettings) error {
-	return getDefaultClient().EditVoiceSettings(voiceId, settings)
-}
-
-func AddVoice(voiceReq AddEditVoiceRequest) (string, error) {
-	return getDefaultClient().AddVoice(voiceReq)
-}
-
-func EditVoice(voiceId string, voiceReq AddEditVoiceRequest) error {
-	return getDefaultClient().EditVoice(voiceId, voiceReq)
-}
-
-func DeleteSample(voiceId, sampleId string) error {
-	return getDefaultClient().DeleteSample(voiceId, sampleId)
-}
-
-func GetSampleAudio(voiceId, sampleId string) ([]byte, error) {
-	return getDefaultClient().GetSampleAudio(voiceId, sampleId)
-}
-
 func NewClient(ctx context.Context, apiKey string, reqTimeout time.Duration) *Client {
 	return &Client{baseURL: elevenlabsBaseURL, apiKey: apiKey, timeout: reqTimeout, ctx: ctx}
 }
